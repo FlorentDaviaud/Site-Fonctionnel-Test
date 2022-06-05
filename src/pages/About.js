2b621dc58497bf323a0dd3a5c5e3ec609057916a
DaviaudNSI <florent.daviaud@gmail.com>
2022-06-05 15:59:54 +0200

    Nouveau site fonctionnel

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non distinctio neque omnis dolore id, aperiam laboriosam ipsam rem architecto voluptatem culpa temporibus illo nam libero perspiciatis aut nostrum doloremque! Consequatur dicta rem ipsa qui tempore quia, culpa veritatis, quas repellat aut quasi harum vitae sequi sapiente commodi recusandae aspernatur consectetur reiciendis nobis suscipit adipisci quo laboriosam et. Dicta magnam quis sunt, voluptatibus numquam veniam quidem, quam accusamus inventore sapiente et beatae, libero labore omnis perspiciatis! Corrupti modi assumenda nesciunt eaque voluptate nisi temporibus cum facere optio ullam sit natus cupiditate obcaecati ratione facilis, totam animi expedita numquam iure esse eveniet?</p>
+            <br />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, veritatis quos odio soluta est porro. Odit illo placeat nobis provident, totam nam quasi beatae adipisci atque molestiae neque temporibus debitis blanditiis a molestias voluptas quae, laudantium aut velit. Nostrum, sed ab ipsum dolor eaque id dolorem fuga voluptas illo perspiciatis. Vitae nobis illum nulla necessitatibus impedit nam aperiam amet iste incidunt temporibus ducimus, explicabo mollitia? Minus in doloremque porro autem quibusdam ullam ducimus voluptatem praesentium officia, beatae vel labore. Delectus qui suscipit accusamus commodi eaque, aspernatur laudantium repellendus animi iste consequuntur hic assumenda vitae unde, ipsum voluptatibus harum molestias veniam!</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+const Home = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>ACCUEIL</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod iste, tempora dolorem inventore delectus obcaecati corporis numquam dicta quae illum error commodi. Facilis exercitationem, incidunt sed earum porro architecto ipsum.</p>
+            <br />
+            <h2>Deuxieme partie</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae aliquam saepe sunt, placeat impedit velit mollitia nobis neque ea sed numquam eius labore excepturi eveniet nihil veniam accusantium quaerat! Delectus.</p>
+            <br />
+
+        </div>
+    );
+};
+
+export default Home;
